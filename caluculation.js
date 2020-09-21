cea0aca230bfc42fb46b540e33a51e39459ab730
pinokapi <63495720+pinokapi@users.noreply.github.com>
2020-09-21 19:02:51 +0900

    新規

---
+++ caluculation.js
@@ -0,0 +1,39 @@
+//変数名はキャメル型で
+//＝は代入演算子だから、比較したいなら===とする
+//関数名はF2を押して編集すると一括で変わる
+//代入変数でなければ、constで定義する
+
+const firstStitch;
+const zougen;
+const secondStitch = firstStitch + zougen;
+
+if (zougen > 0) {
+
+    const i = firstStitch - zougen;
+    if (i >= 0){
+        if ((i % zougen) === 0)　{
+            // (i/zougen)を出力
+        } else {
+            // {Math.floor(i/zougen)} + "余り" + (i % zougen) と出力
+        };
+    } else {
+        //"3目以上の増やし目が必要です"
+    };
+
+} else if (zougen === 0) {
+    //エラー
+
+} else if (0 > zougen) {
+
+    const ab = Math.abs(zougen);
+    const i = firstStitch - ab*2;
+    if (i >= 0){
+        if ((i % ab) === 0)　{
+            // (i/ab)を出力
+        } else {
+            // {Math.floor(i/ab)} + "余り" + (i % ab) と出力
+        };
+    } else {
+        //エラー
+    };
+};
